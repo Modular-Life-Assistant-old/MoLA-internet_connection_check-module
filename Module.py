6ce39b110b93445f993e35250c3ce7787e0a16e9
Treemo <treemo@hotmail.fr>
2015-09-05 19:48:52 +0200

    remove Circuits lib

--- Module.py
+++ Module.py
@@ -1,80 +1,42 @@
-from core import Log
+import socket
+from helpers.modules.BaseModule import BaseModule
 
-from circuits import Component, Event, handler, Timer
+import os
 import subprocess
 import time
 
 
-class internet_up(Event):
-    """Internet up event"""
-    def __init__(self, timestamp): 
-        super(internet_up, self).__init__(timestamp)
+class Module(BaseModule):
+    def run(self):
+        while self.is_running:
+            self._check_down()
+            self._check_up()
 
+    def _check_down(self):
+        up = True
 
-class internet_down(internet_up):
-    """Internet down event"""
+        while up:
+            time.sleep(3)
+            if not self._has_connection():
+                up = False
 
+        self.notify(self._('Internet connection down.'))
 
-class Module(Component):
-    channel = 'internet_connection'
-    __is_commected = True
-
-    def started(self, component):
-        self.check_down()
-
-    def check_down(self):
-        if not self.__check_connection():
-            Log.info('internet_down')
-            self.fire(internet_down(time.time()))
-
-        else:
-            Timer(3, Event.create('check_down')).register(self)
-
-    def check_up(self):
-        if self.__check_connection():
-            Log.info('internet_up')
-            self.fire(internet_up(time.time()))
-
-        else:
-            Timer(1, Event.create('check_up')).register(self)
-
-    def internet_down(self, timestamp):
-        self.check_up()
-
-    def internet_up(self, timestamp):
-        self.check_down()
-
-    def is_connected(self):
-        return self.__is_commected
-
-    def __check_connection(self, count=0):
-        if count <= 2:
-            host = '8.8.8.8'
-        else:
-            host = 'treemo.fr'
+    def _has_connection(self, count=0):
+        host = 'google.com' if count <= 2 else 'github.com'
 
         try:
-            result = subprocess.check_output(
-                ['ping', '-c 1', host]
-             )
-#            time = re.search('time=(.*) ms', result)
-#
-#            if time and count <= 2:
-#                time = float(time.group(1))
-#
-#                if self.__average_time:
-#                    if time >= 3 * self.__average_time:
-#                        Log.error('internet_lag')
-#                        EventManager.trigger('notification', self.lang_get('internet_lag'))
-#                    self.__average_time = (time + self.__average_time) / 2
-#                else:
-#                    self.__average_time = time
-
-            self.__is_commected = True
+            socket.create_connection((host, 80), 2).close()
             return True
         except:
-            if count <= 5:
-                return self.__check_connection(count+1)
-            self.__is_commected = False
-            return False
+            return self._has_connection(count+1) if count <= 5 else False
 
+    def _check_up(self):
+        down = True
+
+        while down:
+            time.sleep(1)
+            if self._has_connection():
+                down = False
+
+        self.notify(self._('Internet connection up.'))
